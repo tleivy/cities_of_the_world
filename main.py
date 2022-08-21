18e916eeb10b4e903e778a6ec86d49309e203c55
to-mer <tomerleivy1@gmail.com>
2022-08-21 18:10:49 +0300

    dev: created population sorted list and 10 biggest cities list #3

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from collections import namedtuple
 
 # creating namedtuple type - cities
-City = namedtuple('City', 'city country population')
+City = namedtuple('City', 'name country population')
 cities = []
 
-# reading cities and creating objects
 with open('cities_of_the_world.csv', 'r', encoding='utf-8') as f:
-    raw_line = f.readline() # skipping header line
-    raw_line = f.readline() # loading first line
+    # reading cities and creating objects
+    raw_line = f.readline()  # skipping header line
+    raw_line = f.readline()  # loading first line
     while raw_line != "":
-        line = raw_line.replace("\"", "").split(",")
-        try:
-            city = City(city=line[0], country=line[4], population=line[9])
-            cities.append(city)
-        except IndexError:
-            print("INDEX ERROR")
+        line = raw_line.replace('","', "|").replace('"', "").split("|")  # cleaning the raw input
+
+        pop_temp = line[9]
+        if pop_temp == "":
+            pop_temp = 0
+        pop_temp = int(float(pop_temp))  # parsing population to int
+
+        curr = City(name=line[0], country=line[4], population=pop_temp)
+        cities.append(curr)
         raw_line = f.readline()
 
-    for i in range(0, 5):
-        print(cities[i])
+    # getting 10 most populated cities
+    population_sorted = sorted(cities, key=lambda city: city[2], reverse=True)
+    ten_biggest_cities = population_sorted[0:10]
+    for city in ten_biggest_cities:
+        print("{}, population: {}".format(city.name, city.population))
+
+
 
 
 
