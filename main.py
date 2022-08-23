463f5a72a306ab9bfdbe71a91f597f993c63bb69
to-mer <tomerleivy1@gmail.com>
2022-08-23 17:23:06 +0300

    feat: made an efficient country list creation #10

--- main.py
+++ main.py
@@ -29,6 +29,33 @@ def population_parse2int(pop_temp):
     return int(float(pop_temp))
 
 
+def update_country_in_list(country_list, city):
+    """
+    this function checks if the current city's country is already in the list.
+    if it isn't then it creates a new country tuple and adds it to the list
+    if it is, updated the population and city counter fields of the country
+    :param country_list:
+    :param city:
+    :return:
+    """
+    in_list = False
+    c_index = 0  # country index
+    for country in country_list:
+        if country[0] == city.country:
+            in_list = True
+            break
+        c_index += 1
+
+    if not in_list:
+        country = Country(name=curr.country, population=curr.population, city_counter=1)
+        country_list.append(country)
+    else:
+        temp_pop = country_list[c_index][1] + city[2]  # adding city population to country total
+        temp_cit = country_list[c_index][2] + 1  # incrementing country's city counter
+        country_list[c_index] = country_list[c_index]._replace(population=temp_pop)
+        country_list[c_index] = country_list[c_index]._replace(city_counter=temp_cit)
+
+
 # creating namedtuple type - City, Country
 City = namedtuple('City', 'name country population')
 Country = namedtuple('Country', 'name population city_counter')
@@ -47,8 +74,18 @@ with open('cities_of_the_world.csv', 'r', encoding='utf-8') as f:
         pop_int = population_parse2int(line[9])
         curr = City(name=line[0], country=line[4], population=pop_int)
         cities.append(curr)
+        update_country_in_list(country_data_lst, curr)
 
         raw_line = f.readline()  # reading the next line of data
+
+    # printing countries (alpha sorted) and number of cities
+    countries_alpha_sorted = sorted(country_data_lst, key=lambda country: country[0])
+    print("")
+    print("countries and number of cities")
+    print("------------------------------")
+    for country in countries_alpha_sorted:
+        print("{}, num of cities: {}".format(country.name, country.city_counter))
+    print("\n")
 
     # getting 10 most populated cities
     population_sorted = sorted(cities, key=lambda city: city[2], reverse=True)
@@ -57,31 +94,6 @@ with open('cities_of_the_world.csv', 'r', encoding='utf-8') as f:
     print("-------------------------")
     for city in ten_biggest_cities:
         print("{}, population: {}".format(city.name, city.population))
-    print("\n")
-    print("\n")
-    
-    countries_sorted = sorted(cities, key=lambda city: city[1])  # list of cities grouped by countries, alphabetical
-    curr_country = countries_sorted[0].country
-
-    city_count = 0
-    pop_sum = 0
-    
-    for city in countries_sorted:
-        if city.country == curr_country:  # if it's still the same country
-            city_count += 1
-            pop_sum += city.population
-        else:
-            curr = Country(name=curr_country, population=pop_sum, city_counter=city_count)  # create a country tuple
-            country_data_lst.append(curr)
-            curr_country = city.country  # update current country and initialize
-            city_count = 1
-            pop_sum = city.population
-
-    print("countries and number of cities")
-    print("------------------------------")
-    for country in country_data_lst:
-        print("{}, num of cities: {}".format(country.name, country.city_counter))    
-    print("\n")
     print("\n")
 
     # finding the ten bottom populated countries
@@ -95,7 +107,7 @@ with open('cities_of_the_world.csv', 'r', encoding='utf-8') as f:
         if counter == 10:
             break    
     
-    print("ten less populated countries")
+    print("ten bottom populated countries")
     print("----------------------------")
     for country in ten_bottom_pop:
         print("{}, population: {}".format(country.name, country.population))
